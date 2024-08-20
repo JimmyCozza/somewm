a47862b66436202aa2ed9807581ffb8eaf5261ce
Jimmy Cozza <moatcozza@gmail.com>
2024-08-20 10:10:24 -0600

    goofing around

--- luaa.c
+++ luaa.c
@@ -3,15 +3,68 @@
 
 static lua_State *L = NULL;
 
-// This is the C function we want to expose to Lua
 static int l_hello_world(lua_State *lua) {
-  printf("Hello from C!\n");
-  return 0; // number of return values
+  printf("Hello, world!\n");
+  return 0;
 }
 
-// This function registers our C functions to Lua
-static void register_lua_functions(lua_State *lua) {
-  lua_register(L, "hello_world", l_hello_world);
+//SOMEWM Library Functions
+// Implementation of the 'some' library functions
+static int l_restart(lua_State *lua) {
+  printf("Restarting...\n");
+  // Add your restart logic here
+  return 0;
+}
+
+static int l_quit(lua_State *lua) {
+  printf("Quitting...\n");
+  // Add your quit logic here
+  return 0;
+}
+
+static int l_spawn(lua_State *lua) {
+  const char *command = luaL_checkstring(L, 1);
+  printf("Spawning: %s\n", command);
+  // Add your spawn logic here
+  return 0;
+}
+
+static const struct luaL_Reg somelib[] = {
+    {"hello_world", l_hello_world},
+    {"restart", l_restart},
+    {"quit", l_quit},
+    {"spawn", l_spawn},
+    {NULL, NULL} // sentinel
+};
+
+//Awful library functions
+//Kill Client Function
+static int l_killclient(lua_State *lua) {
+  // killclient(0);
+  return 0;
+}
+
+static const struct luaL_Reg awfullib[] = {
+  {"killclient", l_killclient},
+  {NULL, NULL}
+};
+
+// Function to register the some library
+static int luaopen_some(lua_State *lua) {
+  luaL_newlib(L, somelib);
+  return 1;
+}
+
+// Function to register the some library
+static int luaopen_awful(lua_State *lua) {
+  luaL_newlib(L, awfullib);
+  return 1;
+}
+
+static void register_libraries(lua_State *lua) {
+  luaL_openlibs(L);
+  luaL_requiref(L, "some", luaopen_some, 1);
+  luaL_requiref(L, "awful", luaopen_awful, 1);
 }
 
 // This function initializes Lua and loads the rc.lua file
@@ -19,26 +72,24 @@ void init_lua(void) {
   if (L != NULL) {
     lua_close(L); // Close any existing Lua state
   }
+
   L = luaL_newstate();
   if (L == NULL) {
     fprintf(stderr, "Failed to create Lua state\n");
     return;
   }
-  luaL_openlibs(L);
 
-  register_lua_functions(L);
+  // luaL_openlibs(L);
+  // luaL_requiref(L, "some", luaopen_some, 1);
+  register_libraries(L);
+  lua_pop(L, 1); // remove the module from the stack (we don't need it anymore)
+  // register_lua_functions(L);
 
   if (luaL_dofile(L, "rc.lua") != LUA_OK) {
     fprintf(stderr, "Error loading rc.lua: %s\n", lua_tostring(L, -1));
     lua_close(L);
     L = NULL;
   }
-    // Load and run the Lua script
-    if (luaL_dofile(L, "basic_drawable.lua") != LUA_OK) {
-        fprintf(stderr, "Error loading basic_drawable.lua: %s\n", lua_tostring(L, -1));
-        lua_close(L);
-        L = NULL;
-    }
 }
 
 int get_config_bool(const char *key, int default_value) {
--- rc.lua
+++ rc.lua
@@ -1,6 +1,11 @@
-general_options = {
-  sloppyfocus = true,
-}
-hello_world()
+-- local draw_surface = require('basic_drawable').draw_surface
 
-print("Hello, world")
+-- draw_surface(800, 600)
+--
+
+general_options = {
+	sloppyfocus = true,
+}
+
+some.hello_world()
+some.quit()
